0ee57f38689eca535866adbc8dfb5387059d21eb
Amir Malehmir <a.malehmir@fanaptelecom.net>
2026-06-13 20:55:38 +0330

    add getter setter for card.java

--- src/main/java/online/shop/binary/card/Card.java
+++ src/main/java/online/shop/binary/card/Card.java
@@ -8,7 +8,20 @@ import online.shop.binary.base.BaseEntity;
 @Entity
 @Table(name = "card")
 public class Card extends BaseEntity {
+	
 	private String products;
 	private String numbers;
-
+	
+	public String getProducts() {
+		return products;
+	}
+	public void setProducts(String products) {
+		this.products = products;
+	}
+	public String getNumbers() {
+		return numbers;
+	}
+	public void setNumbers(String numbers) {
+		this.numbers = numbers;
+	}
 }
