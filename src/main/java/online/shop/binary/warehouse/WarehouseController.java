e4999e51a469e6a21a7682b584a2c5030f70b502
amir <amir@Amir.local>
2026-07-09 20:04:49 +0330

    warehouse - add controller

---
+++ src/main/java/online/shop/binary/warehouse/WarehouseController.java
@@ -0,0 +1,10 @@
+package online.shop.binary.warehouse;
+
+import online.shop.binary.base.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/warehouse")
+public class WarehouseController extends BaseController<Warehouse, WarehouseService> {
+}
