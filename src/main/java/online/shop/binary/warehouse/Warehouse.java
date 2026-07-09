57006908ab6806f0eaed2ffb769e4ab00268b91b
amir <amir@Amir.local>
2026-07-09 19:56:06 +0330

    create warehouse

---
+++ src/main/java/online/shop/binary/warehouse/Warehouse.java
@@ -0,0 +1,11 @@
+package online.shop.binary.warehouse;
+
+import online.shop.binary.base.BaseEntity;
+import online.shop.binary.products.Products;
+
+
+@Entity
+@Table(name = "warehouse")
+public class Warehouse extends BaseEntity {
+
+}
