14260dd36c5ce7b65056066581e42aea99218298
amir <amir@Amir.local>
2026-07-09 20:45:53 +0330

    warehouse - update imports

--- src/main/java/online/shop/binary/warehouse/Warehouse.java
+++ src/main/java/online/shop/binary/warehouse/Warehouse.java
@@ -1,25 +1,23 @@
 package online.shop.binary.warehouse;
 
-import javax.persistence.*;
-
 import online.shop.binary.base.BaseEntity;
 import online.shop.binary.products.Products;
-
+import javax.persistence.*;
 
 @Entity
 @Table(name = "warehouse")
 public class Warehouse extends BaseEntity {
-	@OnetoOne
-	@JoinColumn(name = "product_id", nullable = false, unique = true)
-	private Products product;
-	
-	@Column(nullable = false)
-	private Integer quantity = 0;
-	
-	public Products getProduct() {return product;}
-	public void setProduct(Products product) {this.product = product;}
-	
-	public Integer getQuantity() { return quantity; }
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false, unique = true	)
+    private Products product;
+
+    @Column(nullable = false)
+    private Integer quantity = 0;
+
+    public Products getProduct() { return product; }
+    public void setProduct(Products product) { this.product = product; }
+
+    public Integer getQuantity() { return quantity; }
     public void setQuantity(Integer quantity) { this.quantity = quantity; }
-	
 }
