8df60b0c9277d2d2509acaacd2f7c7a4c82ca141
amir <amir@Amir.local>
2026-07-09 20:03:21 +0330

    warehouse - column + setter/getter

--- src/main/java/online/shop/binary/warehouse/Warehouse.java
+++ src/main/java/online/shop/binary/warehouse/Warehouse.java
@@ -1,5 +1,7 @@
 package online.shop.binary.warehouse;
 
+import javax.persistence.*;
+
 import online.shop.binary.base.BaseEntity;
 import online.shop.binary.products.Products;
 
@@ -7,5 +9,17 @@ import online.shop.binary.products.Products;
 @Entity
 @Table(name = "warehouse")
 public class Warehouse extends BaseEntity {
-
+	@OnetoOne
+	@JoinColumn(name = "product_id", nullable = false, unique = true)
+	private Products product;
+	
+	@Column(nullable = false)
+	private Integer quantity = 0;
+	
+	public Products getProduct() {return product;}
+	public void setProduct(Products product) {this.product = product;}
+	
+	public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+	
 }
