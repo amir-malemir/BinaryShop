86f98f0dc4f684e6840b56fd0c48c95764113ee8
amir <amir@Amir.local>
2026-07-09 20:28:50 +0330

    products - create service

---
+++ src/main/java/online/shop/binary/products/ProductsService.java
@@ -0,0 +1,8 @@
+package online.shop.binary.products;
+
+import online.shop.binary.base.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductsService extends BaseService<Products, ProductsRepository> {
+}
