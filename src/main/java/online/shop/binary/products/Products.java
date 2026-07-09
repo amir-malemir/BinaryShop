7233b5605488694a292977f6b65297fa107295b7
amir <amir@Amir.local>
2026-07-09 20:19:29 +0330

    product - write entity class + setter/getter

--- src/main/java/online/shop/binary/products/Products.java
+++ src/main/java/online/shop/binary/products/Products.java
@@ -1,5 +1,21 @@
 package online.shop.binary.products;
 
-public class Products {
+import java.math.*;
+
+import javax.persistence.*;
+import online.shop.binary.base.BaseEntity;
+
+@Entity
+@Table(name = "products")
+public class Products extends BaseEntity{
+	
+	private String name;
+	private BigDecimal price;
+	
+	public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
 
 }
