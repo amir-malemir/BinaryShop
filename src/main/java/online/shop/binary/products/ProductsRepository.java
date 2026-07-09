d26dde17351916d5e4ee13f03a8835e3e470bd74
amir <amir@Amir.local>
2026-07-09 20:27:28 +0330

    products - create repository

---
+++ src/main/java/online/shop/binary/products/ProductsRepository.java
@@ -0,0 +1,8 @@
+package online.shop.binary.products;
+
+import online.shop.binary.base.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductsRepository extends BaseRepository<Products> {
+}
