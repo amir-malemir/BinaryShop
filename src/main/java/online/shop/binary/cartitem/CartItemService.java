c69712089daafe31b1ab07ae0227076aed3488db
amir <amir@Amir.local>
2026-07-10 23:53:36 +0330

    cartItem - create service

---
+++ src/main/java/online/shop/binary/cartitem/CartItemService.java
@@ -0,0 +1,9 @@
+package online.shop.binary.cartitem;
+
+import org.springframework.stereotype.Service;
+
+import online.shop.binary.base.BaseService;
+
+@Service
+public class CartItemService extends BaseService<CartItem, CartItemRepository> {
+}
