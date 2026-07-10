ecf429b266d1ce848e2bc8075b4ac518192c15c5
amir <amir@Amir.local>
2026-07-10 21:50:58 +0330

    order - create bussinesLogic for create order

--- src/main/java/online/shop/binary/order/OrderService.java
+++ src/main/java/online/shop/binary/order/OrderService.java
@@ -1,8 +1,31 @@
 package online.shop.binary.order;
 
 import online.shop.binary.base.BaseService;
+import online.shop.binary.warehouse.Warehouse;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService extends BaseService<Order, OrderRepository> {
+	
+	
+	@Transactional
+	public void checkout(Long cartId) {
+		Order order = orderRepository.findbyId(cartId);
+		
+		for(OrderItem item:order.getItems()) {
+			Warehouse warehouse = warehouseRepository.findByProduct(item.getProduct());
+			
+			if(warehouse.getQuantity() < item.getQuantity()) {
+				throw new RuntimeException("Stock not enough");
+			}
+			
+		warehouse.setQuantity(warehouse.getQuantity() - item.getQuantity());
+		warehouseRepository.save(warehouse);
+		}
+		order.setStatus("confirmed");
+		orderRepository.save(order);
+	}
+	
 }
