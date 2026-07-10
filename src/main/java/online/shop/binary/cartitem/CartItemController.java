96f6ffcd87a7c6147704ef421469d52bb63804f4
amir <amir@Amir.local>
2026-07-10 23:56:36 +0330

    cartItem - create service

---
+++ src/main/java/online/shop/binary/cartitem/CartItemController.java
@@ -0,0 +1,12 @@
+package online.shop.binary.cartitem;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import online.shop.binary.base.BaseController;
+
+@RestController
+@RequestMapping
+public class CartItemController extends BaseController<CartItem, CartItemService>{
+
+}
