8e824076416b55fa639ca79f8db680a29021536a
amir <amir@Amir.local>
2026-07-13 17:44:54 +0330

    Cart - create Cart Service

--- src/main/java/online/shop/binary/cart/CartService.java
+++ src/main/java/online/shop/binary/cart/CartService.java
@@ -1,9 +1,29 @@
 package online.shop.binary.cart;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import online.shop.binary.base.BaseService;
+import online.shop.binary.cartitem.CartItem;
+import online.shop.binary.warehouse.Warehouse;
+import online.shop.binary.warehouse.WarehouseRepository;
 
 @Service
 public class CartService extends BaseService<Cart, CartRepository> {
-}
+	
+	@Autowired
+	private WarehouseRepository warehouseRepository;
+	
+	@Transactional
+	public void cartDetails(Long cartId) {
+		Cart cart = repository.findById(cartId).orElseThrow(() -> new RuntimeException("Cart not found"));
+		for(CartItem item: cart.getItems()){
+			Warehouse warehouse = warehouseRepository.findByProductId(item.getProduct().getId()).orElseThrow(() -> new RuntimeException("warehouse not found"));
+			if(warehouse.getQuantity() < item.getQuantity()) {
+				throw new RuntimeException("Stock not found");
+			}
+		}
+		
+	}
