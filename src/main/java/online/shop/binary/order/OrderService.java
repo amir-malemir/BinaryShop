d3a7634237a49595dfb666a0243784c072338636
amir <amir@Amir.local>
2026-07-19 19:30:52 +0330

    order - fix package problem

--- src/main/java/online/shop/binary/order/OrderService.java
+++ src/main/java/online/shop/binary/order/OrderService.java
@@ -1,9 +1,5 @@
-itory.findByProductId(cartItem.getProduct().getId()).orElseThrow(() -> new RuntimeException("warehouse not found"));
-			
-			OrderItem orderItem = new OrderItem();
-			orderItem.setProduct(cartItem.getProduct());
-			orderItem.setQuantity(cartItem.getQuantity());
-			orderItem.setOrder(package online.shop.binary.order;
+package online.shop.binary.order;
+
 
 import online.shop.binary.base.BaseService;
 import online.shop.binary.warehouse.*;
@@ -11,6 +7,7 @@ import online.shop.binary.cart.*;
 import online.shop.binary.cartitem.CartItem;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,19 +28,20 @@ public class OrderService extends BaseService<Order, OrderRepository> {
 	@Transactional
 	public Order checkout(Long cartId) {
 		Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new RuntimeException("cart not found"));
+		Order order = repository.findById(cartId).orElseThrow(() -> new RuntimeException("Order not found"));
 		
 		cartService.validateStock(cartId);
-		
-		Order order = new Order();
-		
+				
 		for(CartItem cartItem : cart.getItems()){
-			Warehouse warehouse = warehouseRepository.findByProductId(cartItem.getProduct().getId());
-			if(warehouse.getQuantity() < cartItem.getQuantity()) {
-				throw new RuntimeException("Stock not enough");
-			}
-		
-		warehouse.setQuantity(warehouse.getQuantity() - cartItem.getQuantity());
-		warehouseRepository.save(warehouse);
+			
+			Warehouse warehouse = warehouseRepository.findByProductId(cartItem.getProduct().getId()).orElseThrow(() -> new RuntimeException("Warehouse not found"));
+			OrderItem orderItem = new OrderItem();
+			orderItem.setOrder(order);
+			orderItem.setProduct(cartItem.getProduct());
+			orderItem.setQuantity(cartItem.getQuantity());
+			order.getItems().add(orderItem);
+			warehouse.setQuantity(warehouse.getQuantity() - cartItem.getQuantity());
+			warehouseRepository.save(warehouse);
 		
 		}
 		
@@ -51,23 +49,5 @@ public class OrderService extends BaseService<Order, OrderRepository> {
 		order.setOrderDate(LocalDateTime.now());
 		order.setStatus("confirmed");
 		repository.save(order);
-		
-//		
-//		Order order = repository.findById(cartId)
-//		        .orElseThrow(() -> new RuntimeException("Order not found"));		
-//		for(OrderItem item:order.getItems()) {
-//			Warehouse warehouse = warehouseRepository
-//			        .findByProductId(item.getProduct().getId())
-//			        .orElseThrow(() -> new RuntimeException("Warehouse not found"));			
-//			if(warehouse.getQuantity() < item.getQuantity()) {
-//				throw new RuntimeException("Stock not enough");
-//			}
-//			
-//		warehouse.setQuantity(warehouse.getQuantity() - item.getQuantity());
-//		warehouseRepository.save(warehouse);
-//		}
-//		order.setStatus("confirmed");
-//		repository.save(order);
-//	}
 	}
 }
