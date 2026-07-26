e6b41e15c3137c16ee777634009b651495b42d75
Amir Malehmir <a.malehmir@fanaptelecom.net>
2026-07-26 19:59:17 +0330

    cartRepostiroty - add list for show cart

--- src/main/java/online/shop/binary/cart/CartRepository.java
+++ src/main/java/online/shop/binary/cart/CartRepository.java
@@ -1,8 +1,12 @@
 package online.shop.binary.cart;
 
 import online.shop.binary.base.BaseRepository;
+
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends BaseRepository<Cart> {
+	List<Cart> findByUserIdAndStatus(Long userId, CartStatus status);
 }
