7f9037e3ba636f7d139ff82da755dc287482217d
amir <amir@Amir.local>
2026-07-27 19:17:56 +0330

    create base service impl and use interface- 2

--- src/main/java/online/shop/binary/service/impl/BaseServiceImpl.java
+++ src/main/java/online/shop/binary/service/impl/BaseServiceImpl.java
@@ -1,12 +1,14 @@
 package online.shop.binary.service.impl;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import online.shop.binary.base.BaseEntity;
 import online.shop.binary.base.BaseRepository;
+import online.shop.binary.service.BaseService;
 
-public abstract class BaseServiceImpl<T extends BaseEntity, R extends BaseRepository<T>> {
+public abstract class BaseServiceImpl<T extends BaseEntity, R extends BaseRepository<T>> implements BaseService{
     
     
     protected final R repository;
@@ -15,18 +17,22 @@ public abstract class BaseServiceImpl<T extends BaseEntity, R extends BaseReposi
 		this.repository = repository;
 	}
     
+    @Override
     public List<T> findAll() {
         return repository.findAll();
     }
     
+    @Override
     public Optional<T> findById(Long id) {
         return repository.findById(id);
     }
     
+    @Override
     public T save(T entity) {
         return repository.save(entity);
     }
     
+    @Override
     public void deleteById(Long id) {
         repository.deleteById(id);
     }
