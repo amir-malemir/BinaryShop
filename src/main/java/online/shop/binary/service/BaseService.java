0d80c18989abb860a3af64e953c91e1b4f0e6ca2
amir <amir@Amir.local>
2026-07-27 19:10:05 +0330

    create base service impl and use interface

--- src/main/java/online/shop/binary/service/BaseService.java
+++ src/main/java/online/shop/binary/service/BaseService.java
@@ -1,33 +1,49 @@
+//package online.shop.binary.service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import online.shop.binary.base.BaseEntity;
+//import online.shop.binary.base.BaseRepository;
+//
+//public abstract class BaseService<T extends BaseEntity, R extends BaseRepository<T>> {
+//    
+//    
+//    protected final R repository;
+//    
+//    protected BaseService(R repository) {
+//		this.repository = repository;
+//	}
+//    
+//    public List<T> findAll() {
+//        return repository.findAll();
+//    }
+//    
+//    public Optional<T> findById(Long id) {
+//        return repository.findById(id);
+//    }
+//    
+//    public T save(T entity) {
+//        return repository.save(entity);
+//    }
+//    
+//    public void deleteById(Long id) {
+//        repository.deleteById(id);
+//    }
+//}
+
 package online.shop.binary.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import online.shop.binary.base.BaseEntity;
-import online.shop.binary.base.BaseRepository;
 
-public abstract class BaseService<T extends BaseEntity, R extends BaseRepository<T>> {
-    
-    
-    protected final R repository;
-    
-    protected BaseService(R repository) {
-		this.repository = repository;
-	}
-    
-    public List<T> findAll() {
-        return repository.findAll();
-    }
-    
-    public Optional<T> findById(Long id) {
-        return repository.findById(id);
-    }
-    
-    public T save(T entity) {
-        return repository.save(entity);
-    }
-    
-    public void deleteById(Long id) {
-        repository.deleteById(id);
-    }
+public interface BaseService<T extends BaseEntity>{
+	List<T> findAll();
+	
+	Optional<T> findById(Long id);
+	
+	T save(T entity);
+	
+	void deleteById(Long id);
 }
---
+++ src/main/java/online/shop/binary/service/impl/BaseServiceImpl.java
@@ -0,0 +1,33 @@
+package online.shop.binary.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import online.shop.binary.base.BaseEntity;
+import online.shop.binary.base.BaseRepository;
+
+public abstract class BaseServiceImpl<T extends BaseEntity, R extends BaseRepository<T>> {
+    
+    
+    protected final R repository;
+    
+    protected BaseServiceImpl(R repository) {
+		this.repository = repository;
+	}
+    
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+    
+    public Optional<T> findById(Long id) {
+        return repository.findById(id);
+    }
+    
+    public T save(T entity) {
+        return repository.save(entity);
+    }
+    
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+}
