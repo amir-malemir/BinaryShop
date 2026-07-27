99a1dea1b83184526225ce60626d54851203ab4b
amir <amir@Amir.local>
2026-07-27 20:10:09 +0330

    clean orderService

--- src/main/java/online/shop/binary/service/OrderService.java
+++ src/main/java/online/shop/binary/service/OrderService.java
@@ -2,9 +2,6 @@ package online.shop.binary.service;
 
 import online.shop.binary.order.Order;
 
-
-
-
 public interface OrderService  extends BaseService<Order>{
 	void checkout(Long userId, Long cartId);
 }
