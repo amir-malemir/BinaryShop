755d40e44d01234e06898139dcc828e872df0bb9
amir <amir@Amir.local>
2026-08-30 22:22:33 +0330

    fix userRepository

--- src/main/java/online/shop/binary/users/UserRepository.java
+++ src/main/java/online/shop/binary/users/UserRepository.java
@@ -1,6 +1,9 @@
 package online.shop.binary.users;
 
 import online.shop.binary.base.BaseRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends BaseRepository<User> {
+    User findByUsername(String username);
 }
