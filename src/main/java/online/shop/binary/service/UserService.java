39a40a80bbd67692eeffe720f124255169aa7097
amir <amir@Amir.local>
2026-08-30 21:51:31 +0330

    fix userService

--- src/main/java/online/shop/binary/service/UserService.java
+++ src/main/java/online/shop/binary/service/UserService.java
@@ -1,8 +1,16 @@
 package online.shop.binary.service;
 
 import online.shop.binary.users.User;
+import online.shop.binary.users.dto.UserFilterDto;
+import online.shop.binary.users.dto.UserMergeDto;
+import online.shop.binary.users.dto.UserResponseDto;
+
+import java.util.List;
 
 public interface UserService extends BaseService<User> {
-
     User findByUsername(String username);
+    UserResponseDto saveOrUpdate(UserMergeDto dto);
+    List<UserResponseDto> filter(UserFilterDto filterDto);
+    void deleteAllById(List<Long> ids);
+    UserResponseDto toResponseDto(User entity);
 }
